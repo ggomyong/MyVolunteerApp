5d3fa08311d06923993153c32f83855489717dd8
ggomyong <63125569+ggomyong@users.noreply.github.com>
2020-08-22 10:27:19 -0400

    8/22/2020 lesson

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -9,18 +9,36 @@ import { ProductsService } from 'src/app/services/products.service';
 export class CheckoutComponent implements OnInit {
   
   displayedColumns: string[] = ['name','quantity','price', 'total'];
+
+  destinations = [
+    {value: 'Hospital'},
+    {value: 'Nurinsg home'},
+    {value: 'Orphanage'}
+  ];
+
   datasource = [];
+  totalAmount:number=0;
+  selectedDestination: string='';
 
   constructor(private productService: ProductsService) { }
 
   ngOnInit(): void { //special method invoked at initialization
-    this.datasource=this.productService.getProducts();
-    for (let i=0; i<this.datasource.length; i++) {
-      this.datasource[i].total=this.datasource[i].quantity* this.datasource[i].price;
+    //this.datasource=this.productService.getProducts();
+    let productArray=[];
+
+    productArray=this.productService.getProducts();
+    
+    for (let i=0; i<productArray.length; i++) {
+      //this.datasource[i].total=this.datasource[i].quantity* this.datasource[i].price;
+      if (productArray[i].quantity>0) {
+        productArray[i].total=productArray[i].quantity*productArray[i].price;
+        this.totalAmount=this.totalAmount+productArray[i].total;
+
+        this.datasource.push(productArray[i]);
+      }
     }
-    console.log(this.datasource);
   }
-  //handlePaypal() {
-  //  document.forms["paypalForm"].submit();
-  //}
+  processPaypal() {
+    document.forms["paypalForm"].submit();
+  }
 }
